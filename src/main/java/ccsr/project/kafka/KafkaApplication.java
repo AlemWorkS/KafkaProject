c1285efcabcd59b72ded27b9c3d8b5ccb8898d2a
Degny Gilles <freddegny@gmail.com>
2024-11-07 13:09:20 +0000

    Ajustement des éléments

--- src/main/java/ccsr/project/kafka/KafkaApplication.java
+++ src/main/java/ccsr/project/kafka/KafkaApplication.java
@@ -17,7 +17,7 @@ public class KafkaApplication {
 	@GetMapping("/hello")
 	public String hello(Model model) {
 		model.addAttribute("message","Bienvenue");
-		return "index";
+		return "home";
 	}
 
 
