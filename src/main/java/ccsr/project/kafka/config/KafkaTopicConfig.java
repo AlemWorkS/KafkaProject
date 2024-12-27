3374d8c9d4ceb5e950d45a2efc0b5df105c44c63
Degny Gilles <freddegny@gmail.com>
2024-12-27 00:13:32 +0000

    commission

--- src/main/java/ccsr/project/kafka/config/KafkaTopicConfig.java
+++ src/main/java/ccsr/project/kafka/config/KafkaTopicConfig.java
@@ -1,9 +1,24 @@
 package ccsr.project.kafka.config;
 
+import ccsr.project.kafka.Controllers.DatabaseConnection;
+import ccsr.project.kafka.Controllers.EmailUtil;
+import ccsr.project.kafka.Controllers.KafkaService;
+import ccsr.project.kafka.Controllers.SubscriptionService;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
 
 @Configuration
 public class KafkaTopicConfig {
@@ -11,4 +26,5 @@ public class KafkaTopicConfig {
     public NewTopic baseTopic(){
         return TopicBuilder.name("javaguides").build();
     }
+
 }
