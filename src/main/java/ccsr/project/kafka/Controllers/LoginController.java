c4534cbb597a6e097072170a261a53ee04679b8c
Jojo-225 <eoyotode@gmail.com>
2025-01-06 11:10:04 +0000

    JOJO/DANIE

--- src/main/java/ccsr/project/kafka/Controllers/LoginController.java
+++ src/main/java/ccsr/project/kafka/Controllers/LoginController.java
@@ -1,22 +1,20 @@
 package ccsr.project.kafka.Controllers;
 
-import ccsr.project.kafka.Controllers.UserService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RestController
 public class LoginController {
 
-    private final UserService userService;
+    private final LoginService userService;
 
     @Autowired
-    public LoginController(UserService userService) {
+    public LoginController(LoginService userService) {
         this.userService = userService;
     }
 
--- src/main/java/ccsr/project/kafka/EmailTest.java
+++ src/main/java/ccsr/project/kafka/EmailTest.java
@@ -1,10 +1,12 @@
 package ccsr.project.kafka; // Assurez-vous que ce package correspond à votre projet.
 
+import ccsr.project.kafka.Controllers.EmailConfig;
+
 public class EmailTest {
     public static void main(String[] args) {
         try {
             // Changez l'adresse email, le sujet et le corps pour tester
-            ccsr.project.kafka.Controllers.EmailUtil.sendEmail("eoyotode@gmail.com", "Test Kafka Email", "Ceci est un test d'envoi d'email via Kafka.");
+            EmailConfig.sendEmail("eoyotode@gmail.com", "Test Kafka Email", "Ceci est un test d'envoi d'email via Kafka.");
         } catch (Exception e) {
             e.printStackTrace();
         }
